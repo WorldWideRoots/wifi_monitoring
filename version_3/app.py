8df70714ed506238eaf70b842298c1e8130af4e5
ZiRui Su <raysu1128@gmail.com>
2024-08-26 08:59:18 -0400

    version 3 updates

--- version_3/app.py
+++ version_3/app.py
@@ -1,46 +1,79 @@
 from flask import Flask, jsonify, render_template
-import pandas as pd
 
 app = Flask(__name__)
 
-# Sample data (ensure all columns have the same number of elements)
-device_basic_info = pd.DataFrame({
-    'name': [f'Device{i}' for i in range(1, 101)],
-    'id': [f'{i:03d}' for i in range(1, 101)],
-    'macAddress': [f'00:14:22:01:23:{i:02d}' for i in range(1, 101)],
-    'apType': ['TypeA', 'TypeB', 'TypeC'] * 33 + ['TypeA'],
-    'location': [f'Location{i % 10 + 1}' for i in range(1, 101)],
-    'upTime': ['10:00:00'] * 100,
-    'connectedTime': ['09:30:00'] * 100
-})
-
-df_device_20min_op = pd.DataFrame({
-    'name': [f'Device{i}' for i in range(1, 101)],
-    'connectivityStatus': [0]*30 + [50]*40 + [100]*30,
-    'timestamp': ['2024-08-19 10:00:00'] * 100
-})
-
-# Merge dataframes on 'name'
-df_merged = pd.merge(device_basic_info, df_device_20min_op, on='name')
-
-# Map connectivityStatus to color
-def map_color(status):
-    if status == 100:
-        return 'green'
-    elif status == 0:
-        return 'red'
-    else:
-        return 'yellow'
-
-df_merged['color'] = df_merged['connectivityStatus'].apply(map_color)
+# Sample data for the devices
+device_data = [
+    {
+        "name": "Device_1",
+        "id": "ID_001",
+        "macAddress": "00:1e:0a:59:49:29",
+        "apType": "Type_A",
+        "location": "Location_5",
+        "upTime": 46537,
+        "connectedTime": 4678,
+        "connectivityStatus": 100,
+        "timestamp": "2024-08-22T12:34:56Z",
+        "color": "green"
+    },
+    {
+        "name": "Device_2",
+        "id": "ID_002",
+        "macAddress": "00:0a:37:0f:1e:25",
+        "apType": "Type_B",
+        "location": "Location_2",
+        "upTime": 27782,
+        "connectedTime": 6754,
+        "connectivityStatus": 75,
+        "timestamp": "2024-08-22T12:35:56Z",
+        "color": "yellow"
+    },
+        {
+        "name": "Device_3",
+        "id": "ID_003",
+        "macAddress": "00:56:18:55:04:4e",
+        "apType": "Type_C",
+        "location": "Location_3",
+        "upTime": 49980,
+        "connectedTime": 8013,
+        "connectivityStatus": 50,
+        "timestamp": "2024-08-22T12:36:56Z",
+        "color": "yellow"
+    },
+    {
+        "name": "Device_4",
+        "id": "ID_004",
+        "macAddress": "00:22:16:2c:3c:24",
+        "apType": "Type_A",
+        "location": "Location_8",
+        "upTime": 43991,
+        "connectedTime": 8202,
+        "connectivityStatus": 25,
+        "timestamp": "2024-08-22T12:37:56Z",
+        "color": "red"
+    },
+    {
+        "name": "Device_5",
+        "id": "ID_005",
+        "macAddress": "00:0e:56:13:62:06",
+        "apType": "Type_A",
+        "location": "Location_10",
+        "upTime": 24778,
+        "connectedTime": 6691,
+        "connectivityStatus": 10,
+        "timestamp": "2024-08-22T12:38:56Z",
+        "color": "red"
+    }
+]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/data')
-def get_data():
-    return jsonify(df_merged.to_dict(orient='records'))
+def data():
+    return jsonify(device_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
